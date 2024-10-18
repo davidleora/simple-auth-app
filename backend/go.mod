2814d8f3ebc49ebdce779de32c2c56031449a80d
liuleora@gmail.com <liuleora@gmail.com>
2024-10-18 21:11:19 +0900

    サインアップハンドラーの実装

--- backend/go.mod
+++ backend/go.mod
@@ -1,3 +1,10 @@
 module github.com/davidleora/simple-auth-app/backend
 
 go 1.23.2
+
+require (
+	filippo.io/edwards25519 v1.1.0 // indirect
+	github.com/go-sql-driver/mysql v1.8.1 // indirect
+	github.com/gorilla/mux v1.8.1 // indirect
+	golang.org/x/crypto v0.28.0 // indirect
+)
--- backend/main.go
+++ backend/main.go
@@ -1,13 +1,94 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/gorilla/mux"
+
+	"encoding/json"
+	"math/rand"
+	"time"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
+type User struct {
+	ID               int    `json:"id"`
+	Username         string `json:"username"`
+	Email            string `json:"email"`
+	Password         string `json:"password"`
+	IsVerified       bool   `json:"is_verified"`
+	VerificationCode string `json:"verification_code"`
+}
+
+var db *sql.DB
+
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "Hello, World!")
-	})
-	http.ListenAndServe(":80", nil)
+	// データベースへの接続
+	var err error
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s",
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_NAME"),
+	)
+	db, err = sql.Open("mysql", dsn)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	// ルーターの設定
+	router := mux.NewRouter()
+	router.HandleFunc("/signup", SignUpHandler).Methods("POST")
+
+	// サーバーの起動
+	log.Println("Server is running on port 80")
+	if err := http.ListenAndServe(":80", router); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func SignUpHandler(w http.ResponseWriter, r *http.Request) {
+	var user User
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
+	if err != nil {
+		http.Error(w, "Failed to hash password", http.StatusInternalServerError)
+		return
+	}
+
+	verificationCode := generateVerificationCode()
+
+	_, err = db.Exec("INSERT INTO users (username, email, password, verification_code) VALUES (?, ?, ?, ?)",
+		user.Username, user.Email, string(hashedPassword), verificationCode)
+
+	if err != nil {
+		http.Error(w, "Failed to create user", http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+	fmt.Fprintln(w, "User created successfully")
+}
+
+func generateVerificationCode() string {
+	rand.Seed(time.Now().UnixNano())
+	letters := []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-123456789")
+	b := make([]rune, 32)
+	for i := range b {
+		b[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(b)
 }
